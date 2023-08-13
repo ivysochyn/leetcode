19d5ad9871a00e68c20f9f8a852a90ee38bf3455
Illia Vysochyn <ivysochyn@internships.antmicro.com>
2023-08-14 01:04:27 +0200

    88-merge-sorted-array: More elegant, but less effective solution

--- easy/88-merge-sorted-array/main.py
+++ easy/88-merge-sorted-array/main.py
@@ -11,16 +11,17 @@ class Solution:
         """
         Do not return anything, modify nums1 in place instead.
         """
-        index = 0
-        total_l = m + n
-        while nums2:
-            if nums1[index] > nums2[0]:
-                nums1[index + 1:] = nums1[index:-1]
-                nums1[index] = nums2.pop(0)
-            elif nums1[index] == 0 and (len(nums2) == (total_l - index)):
-                nums1[index:] = nums2[:]
-                break
-            index += 1
+        i, j, k = m - 1, n - 1, m + n - 1
+        while i >= 0 and j >= 0:
+            if (nums1[i] > nums2[j]):
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+        if j >= 0:
+            nums1[k-j:k] = nums2[:j]
         return
 
 
